907fec66afc26cad2f8e2390c963c7117a5ddc4d
zavorinavv <72103150+zavorinavv@users.noreply.github.com>
2020-10-01 21:31:13 +0300

    оо

--- labbb/labbb/labbb.cpp
+++ labbb/labbb/labbb.cpp
@@ -1,20 +1,28 @@
-// labbb.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// labbb.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    float a, b, c, sum, suma_b;
+    printf ("Введите a и b:");
+    scanf ("%F", &a);
+    scanf ("%F", &b);
+    scanf ("%F", &c);
+    suma_b = a + b;
+    sum = c * suma_b * suma_b;
+    if ((c == 0) || (suma_b == 0))
+    {
+        printf("Произведение чисел = 0");
+    }
+    else
+    {
+        printf("Произведение чисел a и b = %F\n", sum);
+    }
+
+
+
+    return 0;
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
