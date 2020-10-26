be10101b7bceeb897a811edbc59e972b2002ccb4
zavorinavv <72103150+zavorinavv@users.noreply.github.com>
2020-10-26 21:01:13 +0300

    лаба 8 задание 8.5

--- labbb/labbb/задание8.5.cpp
+++ labbb/labbb/задание8.5.cpp
@@ -2,27 +2,16 @@
 #include <math.h>
 #include <locale.h>
 int main()
-
 {
 	setlocale(LC_ALL, "Rus");
-	int A[5] = { 5, 9, 6, 1 ,55 };
-	int j, i, x;
-
-	//метод пузырька
-	for (j = 0; j < 5 - 1; j++)
-	{
-		for (i = 0; i < 5 - 1 - j; i++)
-		{
-			if (A[i] > A[i + 1])
-			{
-				x = A[i];
-				A[i] = A[i + 1];
-				A[i + 1] = x;
-			}
-		}
-	}
-	for (int j = 0; j < 5; j++) {
-		printf("%d \n", A[j]);
-	}
+	int a, x, x1, x2, x3, x4;
+	printf("Введите a:", &a);//345- Например
+	scanf_s("%d", &a);
+	x1 = a % 10 * 100;//500
+	x = a % 100;// 45
+	x2 = x / 10 * 10;//40
+	x3 = a / 100;//3
+	x4 = x3 + x2 + x1;
+	printf(" \n %d", x4);
 	return 0;
 }
